1b110b3ebb9e989efbcb7f2b344e77b9679ccadb
Marcos Paulo <itsolutions031250@gmail.com>
2022-08-30 13:14:49 -0300

    adicinado classes e enum

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,3 +1,6 @@
+/*Ler os dados de um trabalhador com N contratos (N fornecido pelo usuário). Depois, solicitar
+do usuário um mês e mostrar qual foi o salário do funcionário nesse mês*/
+
 package application;
 
 public class Program {
